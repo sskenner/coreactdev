c235d709fbb1b5804a5b814632fbd53131cbe387
sk <sskenner@gmail.com>
2019-08-05 19:51:43 -0400

    add promises tuts cont.

--- src/sknotes.js
+++ src/sknotes.js
@@ -1,39 +1,93 @@
-// var promiseTRSANSG = (promiseThatResolvesAfterNSecondsGenerator = function( 
-//     n = 0 
-// ) {
-//     return new Promise(function(resolve, reject) {
-//         setTimeout(function() {
-//             resolve({
-//                 resolvedAfterNSeconds: n
-//             });
-//         }, n * 1000);
-//     });
-// });
-// var promiseTRJANSG = (promiseThatRejectsAfterNSecondsGenerator = function( 
-//     n = 0
-// ) {
-//     return new Promise(function(resolve, reject) {
-//         setTimeout(function() {
-//             reject({
-//                 rejectedAfterNSeconds: n
-//             });
-//         }, n * 1000);
-//     });
-// });
-// case 1
-console.time("Promise.All");
+// case 3
+console.time("Promise.race");
 var promisesArray = [];
-promisesArray.push(promiseTRSANSG(1));
 promisesArray.push(promiseTRSANSG(4));
-promisesArray.push(promiseTRSANSG(2));
-var handleAllPromises = Promise.all(promisesArray);
-handleAllPromises.then(function(values) {
-    console.timeEnd("Promise.All");
-    console.log("All the promises are resolved", values);
+promisesArray.push(promiseTRSANSG(6));
+promisesArray.push(promiseTRSANSG(5));
+promisesArray.push(promiseTRJANSG(3));
+promisesArray.push(promiseTRSANSG(4));
+var promisesRace = Promise.race(promisesArray);
+promisesRace.then(function(values) {
+    console.timeEnd("Promise.race");
+    console.log("the fastest promise resolved", values);
 });
 handleAllPromises.catch(function(reason) {
-    console.log("One of the promises failed with the following reason", reason);
+    console.timeEnd("Promise.race");
+    console.log("the fastest promise rejected with the following reason", reason);
 });
+
+// // case 3
+// console.time("Promise.race");
+// var promisesArray = [];
+// promisesArray.push(promiseTRSANSG(4));
+// promisesArray.push(promiseTRSANSG(3));
+// promisesArray.push(promiseTRSANSG(2));
+// promisesArray.push(promiseTRJANSG(3));
+// promisesArray.push(promiseTRSANSG(4));
+// var promisesRace = Promise.race(promisesArray);
+// promisesRace.then(function(values) {
+//     console.timeEnd("Promise.race");
+//     console.log("the fastest promise resolved", values);
+// });
+// handleAllPromises.catch(function(reason) {
+//     console.timeEnd("Promise.race");
+//     console.log("the fastest promise rejected with the following reason", reason);
+// });
+
+// // case 3
+// console.time("Promise.All");
+// var promisesArray = [];
+// promisesArray.push(promiseTRSANSG(1));
+// promisesArray.push(promiseTRSANSG(5));
+// promisesArray.push(promiseTRSANSG(3));
+// promisesArray.push(promiseTRJANSG(2));
+// promisesArray.push(promiseTRSANSG(4));
+// var handleAllPromises = Promise.all(promisesArray);
+// handleAllPromises.then(function(values) {
+//     console.timeEnd("Promise.All");
+//     console.log("All the promises are resolved", values);
+// });
+// handleAllPromises.catch(function(reason) {
+//     console.timeEnd("Promise.All");
+//     console.log("One of the promises failed with the following reason", reason);
+// });
+
+var promiseTRSANSG = (promiseThatResolvesAfterNSecondsGenerator = function( 
+    n = 0 
+) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve({
+                resolvedAfterNSeconds: n
+            });
+        }, n * 1000);
+    });
+});
+var promiseTRJANSG = (promiseThatRejectsAfterNSecondsGenerator = function( 
+    n = 0
+) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            reject({
+                rejectedAfterNSeconds: n
+            });
+        }, n * 1000);
+    });
+});
+// // case 1 & 2
+// console.time("Promise.All");
+// var promisesArray = [];
+// promisesArray.push(promiseTRSANSG(1));
+// promisesArray.push(promiseTRSANSG(4));
+// promisesArray.push(promiseTRSANSG(2));
+// var handleAllPromises = Promise.all(promisesArray);
+// handleAllPromises.then(function(values) {
+//     console.timeEnd("Promise.All");
+//     console.log("All the promises are resolved", values);
+// });
+// handleAllPromises.catch(function(reason) {
+//     console.log("One of the promises failed with the following reason", reason);
+// });
 ///
 // var promiseTRSANSG = (promiseThatResolvesAfterNSecondsGenerator = function( 
 //     n = 0 
